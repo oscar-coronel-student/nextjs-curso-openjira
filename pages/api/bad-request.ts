f1321365f911bc9c51a521b46a07af1d7174c6e7
Oscar Coronel <135043914+oscar-coronel-student@users.noreply.github.com>
2023-07-25 10:22:19 -0500

    udpate

---
+++ pages/api/bad-request.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    ok: boolean
+    message: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const { message = 'Bad request' } = req.query as { message: string };
+
+    res.status(400).json({
+        ok: false,
+        message
+    })
+}
